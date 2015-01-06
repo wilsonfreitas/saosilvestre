af9a3bbe5f0cc4ffb30bb6601d9d43cf563d5e8a
wilsonfreitas <wilson.freitas@gmail.com>
2015-01-05 22:20:47 -0200

    Many changes to accommodate new examples

--- cetip.py
+++ cetip.py
@@ -1,26 +1,24 @@
 
 import scraps
+
+# Step 1: without apply
+
+class CetipScrap(scraps.Scrap):
+    data = scraps.Attribute(xpath='//*[@id="ctl00_Banner_lblTaxDateDI"]')
+    taxa = scraps.Attribute(xpath='//*[@id="ctl00_Banner_lblTaxDI"]')
+
+scrap = CetipScrap()
+scrap.fetch('http://www.cetip.com.br')
+
+print(scrap.data)
+print(scrap.taxa)
+
+# Step 2: with apply
+
 from datetime import datetime
-
-
-# criar formas funcionais para as funções python
-# pex.
-# str.replace(x, from, to) --> replace(from, to)(x)
-# str.lower(x) --> lower()(x) ou lower...(x)
-
 
 def strptime(format='%Y-%m-%d'):
     return lambda text: datetime.strptime(text, format)
-
-
-foreach = lambda func: lambda seq: [func(x) for x in seq]
-
-
-def _nothing():
-    while True:
-        yield ''
-
-nothing = _nothing()
 
 def replace(_from, _to):
     def _replace(text):
@@ -29,18 +27,15 @@ def replace(_from, _to):
         return text
     return _replace
 
-
 divide_by = lambda x: lambda y: y/x
-
 
 class CetipScrap(scraps.Scrap):
     data = scraps.Attribute(xpath='//*[@id="ctl00_Banner_lblTaxDateDI"]', apply=[
-        replace(')(', nothing), strptime('%d/%m/%Y')
+        strptime('(%d/%m/%Y)')
     ])
     taxa = scraps.Attribute(xpath='//*[@id="ctl00_Banner_lblTaxDI"]', apply=[
-        replace(',%', ['.', '']), float, divide_by(100)
+        replace([',', '%'], ['.', '']), float, divide_by(100)
     ])
-
 
 scrap = CetipScrap()
 scrap.fetch('http://www.cetip.com.br')
